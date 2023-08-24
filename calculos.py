5a1d03df362d7adec6f08eed7887744b76d3ad0b
ThiagoFaligurskidaRocha <138122749+ThiagoFaligurskidaRocha@users.noreply.github.com>
2023-08-24 15:19:45 -0300

    Create calculos.py
    calculos do dia 22/08/2023

---
+++ calculos.py
@@ -0,0 +1,29 @@
+import math
+
+def divisao(x,y):
+    return x/y
+    
+def multiplicacao(x,y):
+    return x*y
+    
+def fatorial(x):
+    aux = 1
+    while x>0:
+        aux = aux*x
+        x=x-1
+    return aux
+    
+def elevacao(x,y):
+    if y == 1:
+        return x
+    else:
+        return x * elevacao(x, y-1)
+
+def raiz(x,y):
+
+def porcentagem(x,y):
+    return (x/100)*y
+
+def logaritmos():
+        return math. log10(x)
+
